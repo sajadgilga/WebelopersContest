1add0b3796618bf41eececd4875af3d5a0296808
aafsari <amirafsari99@yahoo.com>
2018-11-22 12:52:12 +0330

    login added

--- lime/mainpage/views.py
+++ lime/mainpage/views.py
@@ -1,3 +1,23 @@
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
+
 # Create your views here.
+
+def login_(request):
+    if request.POST:
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect("/home")
+        return render(request, "login.html", {"error": True})
+    return render(request, "login.html")
+
+
+@login_required(login_url="/login")
+def home(request):
+    pass
